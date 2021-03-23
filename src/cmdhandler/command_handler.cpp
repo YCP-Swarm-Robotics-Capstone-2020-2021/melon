8e821261719d7544e0149bb0ac159f5cdb482f94
Walrussuit101 <tjefferson@ycp.edu>
2021-03-22 21:15:16 -0400

    remove skeleton comments

--- src/cmdhandler/command_handler.cpp
+++ src/cmdhandler/command_handler.cpp
@@ -395,10 +395,6 @@ std::string command_handler::collector_system(const std::vector<std::string>& to
  */
 std::string command_handler::camera_system(const std::vector<std::string>& tokens, StateVariables& current_state){
     if(tokens[0] == "list"){
-
-        // list out the state variables related to the camera
-        // ex: url, camera_matrix, marker_dictionary, etc.
-
         std::stringstream response;
         response << "Current camera variables:";
 
@@ -427,10 +423,6 @@ std::string command_handler::camera_system(const std::vector<std::string>& token
 
         return response.str();
     }else if(tokens[0] == "set"){
-        // check which variable they want to set (would be in tokens[2])
-        // ex: url, camera_matrix, marker_dictionary, etc.
-
-        // depending on which variable it is, parse out the values from string in tokens[3] and add to current state
         if(tokens.size() < 3){
             return "please provide a variable to set\n    ex: set camera url http://example.com";
         }
@@ -471,10 +463,6 @@ std::string command_handler::camera_system(const std::vector<std::string>& token
 
         return "variable '"+variable+"' does not exist";
     }else if(tokens[0] == "get"){
-        // check which variable they want to get (tokens[2])
-        // ex: url, camera_matrix, marker_dictionary, etc.
-
-        // depending on which variable, get values from current state and add to response string
         if(tokens.size() < 3){
             return "please provide a variable to get\n    ex: get camera url";
         }
@@ -506,10 +494,6 @@ std::string command_handler::camera_system(const std::vector<std::string>& token
 
         return "variable '"+variable+"' does not exist";
     }else if(tokens[0] == "delete"){
-        // check which variable they want to clear (tokens[2])
-        // ex: url, camera_matrix, marker_dictionary, etc.
-
-        // alert user of successful clearing or if they variable they gave doesn't exist
         if(tokens.size() < 3){
             return "please provide a variable to delete\n    ex: delete camera url";
         }
