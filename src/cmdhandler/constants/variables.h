4698394e5f6a25ab2e8f908ebe549f70cefdf667
Walrussuit101 <tjefferson@ycp.edu>
2021-03-30 12:36:53 -0400

    add camera system variable constants

--- src/cmdhandler/constants/variables.h
+++ src/cmdhandler/constants/variables.h
@@ -1,8 +1,14 @@
 #ifndef MELON_VARIABLES_H
 #define MELON_VARIABLES_H
 
-constexpr CAM_URL = "camera_url";
-constexpr CAM_MATRIX = "camera_matrix";
-constexpr CAM_DISTCOEFFS = "camera_dist_coeffs";
+namespace CameraSystemVars
+{
+    constexpr char URL[] = "url";
+    constexpr char CAM_MATRIX[] = "camera_matrix";
+    constexpr char DIST_COEFFS[] = "distortion_matrix";
+    constexpr char MARKER_DICT[] = "marker_dictionary";
+    constexpr char OPTIONS[] = "camera_options";
+}
+
 
 #endif //MELON_VARIABLES_H
