4e298e4b2c5db0f2a449cb508e0bdbf64a47dc61
Earl Kennedy <mnenmenth@gmail.com>
2021-04-03 01:13:31 -0400

    Replace lock_guard occurrences with scoped_lock
    See #43

--- src/cmdhandler/globalstate.cpp
+++ src/cmdhandler/globalstate.cpp
@@ -9,7 +9,9 @@ GlobalState::GlobalState()
 void GlobalState::receive(const StateVariables& state)
 {
     {
-        std::lock_guard<std::mutex> lock(m_mutex);
+        // This lock is within a separate scope than the notify_all function call to ensure that the mutex is
+        // unlocked before notify_all is called
+        std::scoped_lock<std::mutex> lock(m_mutex);
 
         m_state = state;
         m_state.version.store(m_state.version.load()+1);
@@ -22,7 +24,7 @@ bool GlobalState::apply(StateVariables& state)
 {
     if(m_state.version > state.version)
     {
-        std::lock_guard<std::mutex> lock(m_mutex);
+        std::scoped_lock<std::mutex> lock(m_mutex);
         state = m_state;
         return true;
     }
@@ -31,7 +33,7 @@ bool GlobalState::apply(StateVariables& state)
 
 StateVariables GlobalState::get_state()
 {
-    std::lock_guard<std::mutex> lock(m_mutex);
+    std::scoped_lock<std::mutex> lock(m_mutex);
     return m_state;
 }
 
